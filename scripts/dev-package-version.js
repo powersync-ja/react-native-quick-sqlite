d8ffb1542a2212dad57ff19414db98b5b834d84c
Steven Ontong <steven@journeyapps.com>
2023-10-03 15:32:47 +0200

    restore dev package versions

--- scripts/dev-package-version.js
+++ scripts/dev-package-version.js
@@ -1,16 +1,18 @@
 /**
  * Dev package version like 0.0.0-dev-[hash] seem to cause issues with the podspec
  * This script will remove the -dev-hash from the version.
+ * Side Note: Versions above 0.0.0 e.g. 0.0.1-dev-[hash] do work with pod install.
  */
 const fs = require('fs');
 const path = require('path');
-const version = require('../package.json').version;
+const version = process.argv[2];
 
-if (version.includes('-')) {
+if (version.includes('0.0.0')) {
   console.log(`Storing current version for dev version ${version}`);
-
+  const currentPackage = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8'));
+  // Backup the current version
   fs.writeFileSync(
     path.join(__dirname, '../meta.json'),
-    JSON.stringify({ version: version.split('-')[0] }, null, '\t')
+    JSON.stringify({ version: currentPackage.version }, null, '\t')
   );
 }
