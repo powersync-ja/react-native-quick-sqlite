435dd54d4fa5fd0d1f5dd000e2aaa810c361cb31
Steven Ontong <steven@journeyapps.com>
2023-10-03 14:47:04 +0200

    updated postversion script

--- scripts/dev-package-version.js
+++ scripts/dev-package-version.js
@@ -1,17 +1,16 @@
 /**
- * Dev package version like 0.0.0-dev-hash seem to cause issues with the podspec
+ * Dev package version like 0.0.0-dev-[hash] seem to cause issues with the podspec
  * This script will remove the -dev-hash from the version.
  */
 const fs = require('fs');
 const path = require('path');
-const version = process.argv[2];
+const version = require('../package.json').version;
 
-if (version.includes('0.0.0')) {
+if (version.includes('-')) {
   console.log(`Storing current version for dev version ${version}`);
-  const currentPackage = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8'));
-  // Backup the current version
+
   fs.writeFileSync(
     path.join(__dirname, '../meta.json'),
-    JSON.stringify({ version: currentPackage.version }, null, '\t')
+    JSON.stringify({ version: version.split('-')[0] }, null, '\t')
   );
 }
